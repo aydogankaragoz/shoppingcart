3e7fcbf71c803e2a7b2263bb8a0c89e9fad0e79c
Aydogan Karagoz <aydogan.karagoz@gmail.com>
2016-03-12 18:25:21 +0200

    Seperating model into another file

---
+++ model.rb
@@ -0,0 +1,44 @@
+require 'data_mapper'
+
+#MODEL
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/cart.db")
+
+class User
+  include DataMapper::Resource
+  property :username, Text, :key => true
+  property :Name, Text
+  property :email, Text
+  property :password, Text
+
+  has n, :carts
+  has n, :products, :through => :carts
+end
+
+class Cart
+  include DataMapper::Resource
+  property :identifier, Serial
+
+  belongs_to :user
+  has n, :cartitems
+  has n, :products, :through => :cartitems
+end
+
+class Cartitem
+  include DataMapper::Resource
+  property :identifier, Serial
+  property :quantity, Integer
+
+  belongs_to :cart
+  belongs_to :product
+end
+
+class Product
+  include DataMapper::Resource
+  property :name, Text, :key => true
+  property :price, Integer
+
+  has n, :cartitems
+end
+
+DataMapper.finalize.auto_upgrade!
+
