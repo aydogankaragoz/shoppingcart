80e6b8462eb1c678101f1f2c67b6e25e87dcdca7
Aydogan Karagoz <aydogan.karagoz@gmail.com>
2016-03-15 20:09:18 +0200

    Added Json responses for successfull conditions

--- web.rb
+++ web.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
+require 'json'
 
 require './model.rb'
 
 ## Create a new cart
-post '/carts' do
+post '/carts/?' do
   c = Cart.create(:user => User.get(params[:username]) )
+
+  content_type :json
+  { :status => 200, :data => { :cart_id => c.identifier} }.to_json
 end
 
 ## Add product to the cart
@@ -19,9 +23,14 @@ post '/carts/:cart_id/products' do
   cart_id = params[:cart_id]
   product = params[:product]
 
-  Cartitem.create(:cart => Cart.get(params[:cart_id]),
-                  :product => Product.get(params[:product]),
-                  :quantity => quantity)
+  c = Cartitem.create(:cart => Cart.get(params[:cart_id]),
+                      :product => Product.get(params[:product]),
+                      :quantity => quantity)
+
+  content_type :json
+  { :status => 200, :data => { :cartitem_id => c.identifier,
+                               :cartitem_product => c.product_name,
+                               :cartitem_quantity => c.quantity }}.to_json
 end
 
 ## Remove product from the cart
@@ -43,19 +52,29 @@ delete '/carts/:cart_id/products/:product' do
     cartitem.update(:quantity => (cartitem_quantity - remove_quantity))
   end
 
+  content_type :json
+  { :status => 200 }.to_json
+
 end
 
 ## Clean cart
 put '/carts/:cart_id/clean' do
   cartitem = Cartitem.all(:cart => Cart.get(params[:cart_id])).destroy
+
+  content_type :json
+  { :status => 200 }.to_json
 end
 
 ## Set quantity for a product
 put '/carts/:cart_id/products/:product_id' do
 
-  cartitem = Cartitem.first(:product => Product.get(params[:product_id]), :cart => Cart.get(params[:cart_id]))
+  c = cartitem = Cartitem.first(:product => Product.get(params[:product_id]), :cart => Cart.get(params[:cart_id]))
   cartitem.update(:quantity => params[:quantity])
 
+  content_type :json
+  { :status => 200, :data => { :cartitem_id => c.identifier,
+                               :cartitem_product => c.product_name,
+                               :cartitem_quantity => c.quantity }}.to_json
 end
 
 ## Returns detail of the cart
@@ -65,6 +84,8 @@ get '/carts/:cart_id' do
   cartitems.each do |cartitem|
     total += cartitem.quantity * Product.get(cartitem.product_name).price
   end
-  total.to_s
+
+  content_type :json
+  { :status => 200, :data => { :total => total} }.to_json
 end
 
